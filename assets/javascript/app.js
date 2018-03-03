466b78f3bb872ba10837f63706a7f87f993482e2
s3vi26 <s3vi26@outlook.com>
2018-03-02 20:40:35 -0800

    updated, still working

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+$( document ).ready(function () {
+    let game = {
+        time: "",
+        correct: "",
+        incorrect: "",
+        unanswered: "",
+        ans_array: "",    
+        start:
+            function start(){
+                $(".start_screen").hide()
+                $(".game_content").show()
+                game.time = 5
+                $("#time").text(game.time)
+                game.display_time()
+            },
+        display_time:
+            function display_time() {
+                intervalId = setInterval(game.countdown, 1000)
+                $("#time").text(game.time)   
+        },
+        countdown:
+            function countdown() {
+                game.time--
+                $("#time").text(game.time)
+                if (game.time === 0) {
+                    clearInterval(intervalId)
+                    game.results()
+                    $(".game_content").hide()
+                    $(".game_over").show()
+                }
+            },
+        results:
+            function results () {
+                array = $("input:checked")
+                    game.ans_array = array
+                    game.score()
+                },
+        score:
+            function score() {
+                for (i=0; i<game.ans_array.length; i++) {
+                    console.log(this.ans_array[i])
+    
+                }
+            },    
+    }
+    $("#start_button").on("click", game.start)
+    start_screen()
+    function start_screen () {
+        $(".game_content").hide()
+        $(".game_over").hide()
+    }
+    })
